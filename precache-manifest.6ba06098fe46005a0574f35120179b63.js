10ed07391e08b5d24f5e15b3d9a20c3454f4d8bf
JVictorV <40485924+JVictorV@users.noreply.github.com>
2019-03-02 20:24:56 -0300

    Updates

---
+++ precache-manifest.6ba06098fe46005a0574f35120179b63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "308e8a26ce7019366853",
+    "url": "/Calculator/static/css/main.e7e6d1a9.chunk.css"
+  },
+  {
+    "revision": "308e8a26ce7019366853",
+    "url": "/Calculator/static/js/main.308e8a26.chunk.js"
+  },
+  {
+    "revision": "c2a97d8d426c7554df3f",
+    "url": "/Calculator/static/js/runtime~main.c2a97d8d.js"
+  },
+  {
+    "revision": "e5d8099e8c1a29addaf8",
+    "url": "/Calculator/static/js/2.e5d8099e.chunk.js"
+  },
+  {
+    "revision": "f08282f0b3ae8562d91d32b5c91a1985",
+    "url": "/Calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/calculator/precache-manifest.56cf64efc512810e6888297ab537ca3b.js"
+  "/Calculator/precache-manifest.6ba06098fe46005a0574f35120179b63.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/calculator/index.html", {
+workbox.routing.registerNavigationRoute("/Calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
